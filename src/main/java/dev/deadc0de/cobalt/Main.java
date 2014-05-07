015ef6ec9bc24d431cd842b27c49cd807d3e86b2
labes <0x1abe5@gmail.com>
2014-05-07 19:47:58 +0200

    managing main character in World class

--- src/main/java/dev/deadc0de/cobalt/Main.java
+++ src/main/java/dev/deadc0de/cobalt/Main.java
@@ -5,26 +5,17 @@ import dev.deadc0de.cobalt.geometry.Point;
 import dev.deadc0de.cobalt.geometry.Region;
 import dev.deadc0de.cobalt.graphics.ImmutableView;
 import dev.deadc0de.cobalt.graphics.MovableView;
-import dev.deadc0de.cobalt.graphics.MutableSprite;
-import dev.deadc0de.cobalt.graphics.Sprite;
 import dev.deadc0de.cobalt.graphics.javafx.JavaFXRenderingStack;
 import dev.deadc0de.cobalt.input.KeyboardInputFocusStack;
 import dev.deadc0de.cobalt.text.SpriteTextFacade;
 import dev.deadc0de.cobalt.text.TextInput;
-import dev.deadc0de.cobalt.world.MainCharacterController;
-import dev.deadc0de.cobalt.world.MainCharacterElement;
-import dev.deadc0de.cobalt.world.PositionTracker;
 import dev.deadc0de.cobalt.world.World;
-import dev.deadc0de.cobalt.world.ZoneEnvironment;
 import dev.deadc0de.cobalt.world.ZoneInput;
 import java.util.ArrayList;
 import java.util.EnumMap;
-import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.function.Supplier;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
@@ -42,6 +33,7 @@ public class Main extends Application {
     private static final int HEIGHT = 9;
     private static final int TILE_SIZE = 16;
     private static final Dimension RENDERING_AREA = new Dimension(WIDTH * TILE_SIZE, HEIGHT * TILE_SIZE);
+    private static final Point VIEW_RELATIVE_POSITION = new Point(- 4 * TILE_SIZE, - 4 * TILE_SIZE + 4);
 
     private final List<Updatable> updateHandlers = new ArrayList<>();
     private final MovableView view = new MovableView(0, 0, RENDERING_AREA);
@@ -59,8 +51,11 @@ public class Main extends Application {
         graphics = new JavaFXRenderingStack(root.getChildren(), imagesRepository::get, spritesRegionsRepository::get);
         textFacade = new SpriteTextFacade(graphics, new ImmutableView(new Region(new Point(0, 0), RENDERING_AREA)), input);
         updateHandlers.add(textFacade);
-        world = new World(textFacade, imagesRepository::put, spritesRegionsRepository::put, graphics, this::mainCharacterEnvironment);
-        updateHandlers.add(world.pushZone("pallet-town", view));
+        world = new World(textFacade, imagesRepository::put, spritesRegionsRepository::put, graphics, input, VIEW_RELATIVE_POSITION);
+        final int initialRow = 9;
+        final int initialColumn = 8;
+        final Point initialPosition = new Point(initialColumn * TILE_SIZE, initialRow * TILE_SIZE - 4);
+        updateHandlers.add(world.pushZone("pallet-town", 9, 8, initialPosition, view));
         updateHandlers.add(graphics);
     }
 
@@ -124,23 +119,6 @@ public class Main extends Application {
         repository.putAll(Sprites.spritesRegions());
         repository.putAll(MainCharacter.spritesRegions());
         return repository;
-    }
-
-    private Sprite mainCharacterEnvironment(ZoneEnvironment environment) {
-        final Point initialPosition = new Point(8 * TILE_SIZE, 9 * TILE_SIZE - 4);
-        final MutableSprite mainCharacterSprite = new MutableSprite(null, initialPosition);
-        view.x = initialPosition.x - 4 * TILE_SIZE;
-        view.y = initialPosition.y - 4 * TILE_SIZE + 4;
-        final PositionTracker onCharacterMoved = (x, y) -> {
-            mainCharacterSprite.setPosition(mainCharacterSprite.position().add(new Point(x, y)));
-            view.x += x;
-            view.y += y;
-        };
-        final MainCharacterElement mainCharacterElement = new MainCharacterElement(mainCharacterSprite::setState, onCharacterMoved);
-        final Supplier<Set<ZoneInput>> activeInputProvider = input.pushFocus(ZoneInput.class, () -> EnumSet.noneOf(ZoneInput.class));
-        final MainCharacterController manager = new MainCharacterController(mainCharacterElement, activeInputProvider, environment, 9, 8);
-        updateHandlers.add(manager);
-        return mainCharacterSprite;
     }
 
     private void startRendering() {
--- src/main/java/dev/deadc0de/cobalt/world/World.java
+++ src/main/java/dev/deadc0de/cobalt/world/World.java
@@ -1,18 +1,22 @@
 package dev.deadc0de.cobalt.world;
 
 import dev.deadc0de.cobalt.Updatable;
+import dev.deadc0de.cobalt.geometry.Point;
 import dev.deadc0de.cobalt.geometry.Region;
+import dev.deadc0de.cobalt.graphics.MovableView;
+import dev.deadc0de.cobalt.graphics.MutableSprite;
 import dev.deadc0de.cobalt.graphics.OverlayingSpritesLayer;
 import dev.deadc0de.cobalt.graphics.RenderingLayer;
 import dev.deadc0de.cobalt.graphics.RenderingStack;
-import dev.deadc0de.cobalt.graphics.Sprite;
-import dev.deadc0de.cobalt.graphics.View;
+import dev.deadc0de.cobalt.input.InputFocusStack;
 import dev.deadc0de.cobalt.text.TextFacade;
+import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ServiceLoader;
+import java.util.Set;
 import java.util.function.BiConsumer;
-import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 import javafx.scene.image.Image;
 
@@ -20,13 +24,15 @@ public class World {
 
     private final Map<String, Zone> zones;
     private final RenderingStack graphics;
-    private final Function<ZoneEnvironment, Sprite> mainCharacterSpriteFactory;
+    private final InputFocusStack input;
+    private final Point viewRelativePosition;
     private Zone currentZone;
 
-    public World(TextFacade textFacade, BiConsumer<String, Image> imagesRepository, BiConsumer<String, Region> spritesRegionsRepository, RenderingStack graphics, Function<ZoneEnvironment, Sprite> mainCharacterSpriteFactory) {
+    public World(TextFacade textFacade, BiConsumer<String, Image> imagesRepository, BiConsumer<String, Region> spritesRegionsRepository, RenderingStack graphics, InputFocusStack input, Point viewRelativePosition) {
         zones = new HashMap<>();
         this.graphics = graphics;
-        this.mainCharacterSpriteFactory = mainCharacterSpriteFactory;
+        this.input = input;
+        this.viewRelativePosition = viewRelativePosition;
         loadZones(textFacade, imagesRepository, spritesRegionsRepository);
     }
 
@@ -38,22 +44,44 @@ public class World {
         }
     }
 
-    public Updatable pushZone(String name, View view) {
+    public Updatable pushZone(String name, int row, int column, Point mainCharacterPosition, MovableView view) {
         if (currentZone != null) {
             popZone();
         }
         currentZone = zones.get(name);
-        final Sprite mainCharacterSprite = mainCharacterSpriteFactory.apply(currentZone.environment);
+        final MutableSprite mainCharacterSprite = createMainCharacterSprite(mainCharacterPosition, view);
+        final MainCharacterController mainCharacterController = createMainCharacterController(mainCharacterSprite, currentZone.environment, row, column, view);
         final RenderingLayer layer = new OverlayingSpritesLayer(new ZoneRenderingLayer(currentZone), () -> Stream.of(mainCharacterSprite));
         graphics.pushLayer(layer, view);
         final Zone zoneReference = currentZone;
         return () -> {
             zoneReference.updatables.get().forEach(Runnable::run);
+            mainCharacterController.update();
         };
+    }
+
+    private MutableSprite createMainCharacterSprite(Point mainCharacterPosition, MovableView view) {
+        final MutableSprite mainCharacterSprite = new MutableSprite(null, mainCharacterPosition);
+        view.x = mainCharacterPosition.x + viewRelativePosition.x;
+        view.y = mainCharacterPosition.y + viewRelativePosition.y;
+        return mainCharacterSprite;
+    }
+
+    private MainCharacterController createMainCharacterController(MutableSprite mainCharacterSprite, ZoneEnvironment environment, int row, int column, MovableView view) {
+        final PositionTracker onCharacterMoved = (x, y) -> {
+            mainCharacterSprite.setPosition(mainCharacterSprite.position().add(new Point(x, y)));
+            view.x += x;
+            view.y += y;
+        };
+        final MainCharacterElement mainCharacterElement = new MainCharacterElement(mainCharacterSprite::setState, onCharacterMoved);
+        final Supplier<Set<ZoneInput>> activeInputProvider = input.pushFocus(ZoneInput.class, () -> EnumSet.noneOf(ZoneInput.class));
+        final MainCharacterController controller = new MainCharacterController(mainCharacterElement, activeInputProvider, environment, row, column);
+        return controller;
     }
 
     private void popZone() {
         currentZone = null;
+        input.popFocus();
         graphics.popLayer();
     }
 
